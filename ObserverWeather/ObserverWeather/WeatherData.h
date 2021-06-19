71dd9f4270b636abce6439feff56d807bafc1501
wanghongsheng01 <2496533749@qq.com>
2021-06-19 15:16:07 +0800

    observer_weather

---
+++ ObserverWeather/ObserverWeather/WeatherData.h
@@ -0,0 +1,45 @@
+//
+//  WeatherData.h
+//  ObserverWeather
+//
+//  Created by hswang on 2021/6/19.
+//
+
+#ifndef WeatherData_h
+#define WeatherData_h
+
+#include "Subject.h"
+#include "Observer.h"
+
+#include <unordered_set>
+
+class WeatherData : public Subject{
+    
+public:
+    void registerObserver(Observer* o) override{
+        observers.insert(o);
+    }
+    
+    void removeObserver(Observer* o) override{
+        observers.erase(o);
+    }
+    
+    void notifyObserver() override{
+        for(auto o:observers){
+            o->update_from_weather(Temperature, Humidity);
+        }
+    }
+    
+    void setMeasurements(float t, float h){
+        Temperature = t;
+        Humidity = h;
+        notifyObserver();
+    }
+    
+private:
+    float Temperature;
+    float Humidity;
+    std::unordered_set<Observer*> observers;
+};
+
+#endif /* WeatherData_h */
